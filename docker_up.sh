19d9d2e05c8401fa75c0fe5a6399833fd17e8919
Pablo Cerda <pablo@citla.com>
2018-02-25 20:19:13 +0100

    Creation of solr storage volume and container setup.

--- docker_up.sh
+++ docker_up.sh
@@ -2,7 +2,24 @@
 
 # Service name defined in docker-compose.yml
 SERVICE_NAME=${1}
+VOLUME_NAME="volume_${SERVICE_NAME}"
 
+if [[ "$SERVICE_NAME" == '' ]]; then
+  echo "The argument service name is required."
+  exit 1
+fi
+
+echo "Creating '${VOLUME_NAME}'..."
+
+# Create the service volume.
+docker volume create --name=${VOLUME_NAME}
+docker create --rm --name temporal --user root -v ${VOLUME_NAME}:/search solr chown -R 8983:8983 /search
+
+echo "Copying config files..."
+docker cp ./config/drupal7/core.properties temporal:/search/core.properties
+docker cp ./config/drupal7/solr-6.x temporal:/search/conf
+docker cp ./data temporal:/search/data
+docker start temporal
+
+echo "Composing '${SERVICE_NAME}' service container."
 docker-compose up ${SERVICE_NAME}
-# Set core data permissions to solr user / group.
-docker-compose exec --user=root ${SERVICE_NAME} chown -R solr:solr /opt/solr/server/solr/mycores/search/data
